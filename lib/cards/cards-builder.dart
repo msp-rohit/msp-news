bc96e5f9db9a3bf6ab02ecf8eedc1edfdadac078
Pushkar DK <pushkar@mysmartprice.com>
2018-07-30 19:37:35 +0530

    Basic animation done

--- lib/cards/cards-builder.dart
+++ lib/cards/cards-builder.dart
@@ -10,51 +10,167 @@ class CardsBuilder extends StatefulWidget {
   _CardsBuilderState createState() => _CardsBuilderState();
 }
 
-class _CardsBuilderState extends State<CardsBuilder> {
+class _CardsBuilderState extends State<CardsBuilder> with TickerProviderStateMixin {
   int index;
+  bool showCurCard;
+  bool showPrevCard;
+  bool showNextCard;
+
+  double prevCardY;
+  double tempPrevCardY;
+  double stackCardY;
+  double tempStackCardY;
+  double yStartOffset;
+  double yEndOffset;
+
+  AnimationController yAnimationUp;
+  AnimationController yAnimationDown;
+  AnimationController yFlyOutUp;
+  AnimationController yFlyInDown;
 
   @override
   void initState() {
     super.initState();
-    index = 0;
+    index = -1;
+    showPrevCard = index >= 0;
+    showCurCard = index + 1 < widget.cards.length;
+    showNextCard = index + 2 < widget.cards.length;
+
+    prevCardY = -750.0;
+    tempPrevCardY = -750.0;
+    stackCardY = 0.0;
+    tempStackCardY = 0.0;
+
+    /* Local variables */
+    double slideUp = 250.0;
+    double slideDown = 250.0;
+    double flyInMovement = 700.0;
+    double flyOutMovement = 700.0;
+
+    /* Animation Controllers */
+    yAnimationUp = new AnimationController(duration: const Duration(milliseconds: 220), vsync: this);
+    yAnimationUp.addListener(() {
+      if(index != widget.cards.length - 1) {
+        setState(() {
+          stackCardY = yAnimationUp.value * -slideUp;
+        });
+      }
+    });
+    yAnimationDown = new AnimationController(duration: const Duration(milliseconds: 160), vsync: this);
+    yAnimationDown.addListener(() {
+      if(index != -1) {
+        setState(() {
+          prevCardY = tempPrevCardY + (yAnimationDown.value * flyInMovement);
+        });
+      }
+    });
+    yFlyOutUp = new AnimationController(duration: const Duration(milliseconds: 320), vsync: this);
+    yFlyOutUp.addListener(() {
+      /* Next card */
+      if(index != widget.cards.length - 1) {
+        setState(() {
+          stackCardY = tempStackCardY - (yFlyOutUp.value * flyOutMovement);
+          if(yFlyOutUp.value == 1) {
+            index++;
+            resetParams();
+          }
+        });
+      }
+    });
+    yFlyInDown = new AnimationController(duration: const Duration(milliseconds: 320), vsync: this);
+    yFlyInDown.addListener(() {
+      /* Prev card */
+      if(index > -1) {
+        setState(() {
+          prevCardY = tempPrevCardY + (yFlyInDown.value * slideDown);
+          if(yFlyInDown.value == 1) {
+            index--;
+            resetParams();
+          }
+        });
+      }
+    });
   }
 
-  void switchPages(details) {
-    if(details.primaryVelocity < -1000) {
-      setState(() {
-        index != (widget.cards.length) ? index++ : index; // Next Card (move window forward)
-        // Card @ widget.cards.length is 'end of articles' card: Hence, displaying it.
-        // Above is the reason for not using `index != (widget.cards.length - 1)`
-      });
-    } else if(details.primaryVelocity > 1000) {
-      setState(() {
-        index != 0 ? index--: index; // Previous Card (move window backward)
-      });
+  /* Custom Functions: */
+  void resetParams() {
+    showPrevCard = index >= 0;
+    showCurCard = index + 1 < widget.cards.length;
+    showNextCard = index + 2 < widget.cards.length;
+
+    prevCardY = -750.0;
+    tempPrevCardY = -750.0;
+    stackCardY = 0.0;
+    tempStackCardY = 0.0;
+  }
+
+  void toNextCardY() {
+    if(index < widget.cards.length - 1) {
+      tempStackCardY = stackCardY;
+      yFlyOutUp.forward(from: 0.0);
+    } else {
+      yAnimationUp.reverse(from: 1.0);
+    }
+  }
+
+  void toPrevCardY() {
+    if(index > -1) {
+      tempPrevCardY = prevCardY;
+      yFlyInDown.forward(from: 0.0);
+    } else {
+      yAnimationDown.reverse(from: 1.0);
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector( 
+    return GestureDetector(
+      onTap: () { /* Do nothing */ },
       onVerticalDragStart: (details) {
-        print("*Start Vertical: Direction = ${details.globalPosition.direction}");
+        yStartOffset = details.globalPosition.dy;
+        yEndOffset = yStartOffset;
+        if(details.globalPosition.direction >= 1) { // Upwards
+          yAnimationUp.forward(from: 0.0);
+        } else { // Downwards
+          yAnimationDown.forward(from: 0.0);
+        }
+      },
+      onVerticalDragUpdate: (details) {
+        yEndOffset = details.globalPosition.dy;
       },
       onVerticalDragEnd: (details) {
-        print("*End*");
-        print("Primary Velocity: ${details.primaryVelocity}");
-        switchPages(details);
-      },
-      onHorizontalDragStart: (details) {
-        print("*Start Horizontal: Direction = ${details.globalPosition.direction}");
-      },
-      onHorizontalDragEnd: (details) {
-        print("*End*");
-        print("Primary Velocity: ${details.primaryVelocity}");
-        switchPages(details);
+        double distance = yEndOffset - yStartOffset;
+        double thresholdValue = 70.0;
+        // 1. If card was swiped quickly then remove it!
+        if(details.primaryVelocity != 0) {
+          if(distance < 0) { // upwards / next card
+            toNextCardY();
+          } else { // Downwards / prev card
+            toPrevCardY();
+          }
+        } 
+        // 2. If card was swiped slowly then check for distance threshold
+        else {
+          // 2a. If threshold was crossed: complete the action
+          if(distance.abs() > thresholdValue) {
+            if(distance < 0) { // upwards / next card
+              toNextCardY();
+            } else { // Downwards / prev card
+              toPrevCardY();
+            }
+          } else {
+            // 2b. If threshold was not crossed: reverse the animation
+            if(distance < 0) { // upwards
+              yAnimationUp.reverse(from: 1.0);
+            } else { // Downwards
+              yAnimationDown.reverse(from: 1.0);
+            }
+          }
+        }
       },
       child: Stack(
         children: <Widget>[
-          /* Last card: For border/shadow effects */
+          /* Last card (Static): For border/shadow effects */
           Container(
             margin: new EdgeInsets.only(
               top: 30.0,
@@ -81,10 +197,21 @@ class _CardsBuilderState extends State<CardsBuilder> {
               )
             )
           ),
-          /* Real Card(s): */
-          IndividualCard(
-            newsItem: widget.cards[index == widget.cards.length ? index - 1 : index]
-          )
+          /* Next Card: */
+          showNextCard ? IndividualCard(
+            newsItem: widget.cards[index + 2] ,
+            positionY: 0.0 // Remains fixed (No animations affect this card => static)
+          ) : Container(width: 0.0, height: 0.0),
+          /* Current Card: */
+          showCurCard ? IndividualCard(
+            newsItem: widget.cards[index + 1] ,
+            positionY: stackCardY
+          ) : Container(width: 0.0, height: 0.0),
+          /* Previous Card: */
+          showPrevCard ? IndividualCard(
+            newsItem: widget.cards[index],
+            positionY: prevCardY
+          ) : Container(width: 0.0, height: 0.0),
         ]
       )
     );
--- lib/cards/individual-card.dart
+++ lib/cards/individual-card.dart
@@ -4,8 +4,8 @@ import 'package:url_launcher/url_launcher.dart';
 
 class IndividualCard extends StatefulWidget {
   final News newsItem;
-  final double position;
-  IndividualCard({Key key, this.newsItem, this.position}) : super(key: key);
+  final double positionY;
+  IndividualCard({Key key, this.newsItem, this.positionY}) : super(key: key);
   
   @override
   _IndividualCardState createState() => _IndividualCardState();
@@ -14,120 +14,132 @@ class IndividualCard extends StatefulWidget {
 class _IndividualCardState extends State<IndividualCard> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-      elevation: 0.0,
-      margin: new EdgeInsets.only(
-        top: 30.0,
-        bottom: 10.0,
-        left: 10.0,
-        right: 10.0
-      ),
-      color: const Color(0xFFFFFFFF),
-      child: Column(
-        children: <Widget>[
-          /* Image: */
-          Flexible(
-            flex: 3,
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(widget.newsItem.imageUrl),
-                  fit: BoxFit.cover,
+    return Positioned(
+      top: widget.positionY,
+      left: 0.0,
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Card(
+        shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        elevation: 0.0,
+        margin: new EdgeInsets.only(
+          top: 30.0,
+          bottom: 10.0,
+          left: 10.0,
+          right: 10.0
+        ),
+        color: const Color(0xFFFFFFFF),
+        child: Column(
+          children: <Widget>[
+            /* Image: */
+            Flexible(
+              flex: 3,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(widget.newsItem.imageUrl),
+                    fit: BoxFit.cover,
+                  )
                 )
               )
-            )
-          ),
-          /* News Source: */
-          Container(
-            padding: new EdgeInsets.only(top: 10.0, bottom: 5.0, right: 10.0, left: 10.0),
-            width: double.infinity,
-            child: Text(widget.newsItem.sourceName, style: TextStyle(
-                fontSize: 11.0,
-                fontFamily: 'Roboto'
-              ),
-              overflow: TextOverflow.fade
-            )
-          ),
-          /* News Title: */
-          Container(
-            padding: new EdgeInsets.only(bottom: 5.0, left: 10.0, right: 10.0),
-            width: double.infinity,
-            child: Text(widget.newsItem.title, style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.w500
-              ),
-              maxLines: 3,
-              overflow: TextOverflow.ellipsis
-            )
-          ),
-          /* Publish Date: */
-          Container(
-            padding: new EdgeInsets.only(bottom: 10.0, right: 10.0, left: 10.0),
-            width: double.infinity,
-            child: Text(widget.newsItem.publishedDate, style: TextStyle(
-                fontSize: 11.0,
-                fontFamily: 'Roboto'
-              ), 
-              overflow: TextOverflow.fade
-            )
-          ),
-          /* Description: */
-          Expanded(
-            child: Container(
-              padding: new EdgeInsets.only(bottom: 15.0, right: 10.0, left: 10.0),
+            ),
+            /* News Source: */
+            Container(
+              padding: new EdgeInsets.only(top: 10.0, bottom: 5.0, right: 10.0, left: 10.0),
               width: double.infinity,
-              child: Text(widget.newsItem.description, style: TextStyle(
-                  fontSize: 15.0,
-                  height: 1.3,
+              child: Text(widget.newsItem.sourceName, style: TextStyle(
+                  fontSize: 11.0,
                   fontFamily: 'Roboto'
                 ),
                 overflow: TextOverflow.fade
               )
-            )
-          ),
-          /* Article Link Button: */
-          Container(
-            decoration: BoxDecoration(
-              gradient: new LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  const Color(0x00FFFFFF), const Color(0xDDFFFFFF), 
-                  const Color(0xEEFFFFFF), const Color(0xFFFFFFFF),
-                  const Color(0xFFFFFFFF)
+            ),
+            /* News Title: */
+            Container(
+              padding: new EdgeInsets.only(bottom: 5.0, left: 10.0, right: 10.0),
+              width: double.infinity,
+              child: Text(widget.newsItem.title, style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w500
+                ),
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis
+              )
+            ),
+            /* Publish Date: */
+            Container(
+              padding: new EdgeInsets.only(bottom: 10.0, right: 10.0, left: 10.0),
+              width: double.infinity,
+              child: Row(
+                children: <Widget>[
+                  Icon(const IconData(0xe916, fontFamily: 'MaterialIcons'), size: 11.0),
+                  Padding(padding: new EdgeInsets.all(1.0)),
+                  Text(widget.newsItem.publishedDate, style: TextStyle(
+                      fontSize: 11.0,
+                      fontFamily: 'Roboto'
+                    ), 
+                    overflow: TextOverflow.fade
+                  )
                 ],
-                tileMode: TileMode.repeated, // repeats the gradient over the canvas
-                stops: [0.0, 0.25, 0.5, 0.75, 1.0]
               ),
             ),
-            width: double.infinity,
-            height: 90.0,
-            child: Center(
-              child: RaisedButton(
-                onPressed: () async {
-                  if (await canLaunch(widget.newsItem.fullArticleLink)) {
-                    await launch(widget.newsItem.fullArticleLink);
-                  } else {
-                    throw 'Could not launch ${widget.newsItem.fullArticleLink}';
-                  }
-                },
-                padding: new EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
-                shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-                textColor: Colors.black,
-                color: Colors.white.withOpacity(1.0),
-                child: Text(
-                  'View Article',
-                  style: TextStyle(
-                    color: const Color(0xFF2980b9),
-                    fontSize: 12.0,
-                    fontWeight: FontWeight.w500
-                  )
+            /* Description: */
+            Expanded(
+              child: Container(
+                padding: new EdgeInsets.only(bottom: 15.0, right: 10.0, left: 10.0),
+                width: double.infinity,
+                child: Text(widget.newsItem.description, style: TextStyle(
+                    fontSize: 15.0,
+                    height: 1.3,
+                    fontFamily: 'Roboto'
+                  ),
+                  overflow: TextOverflow.fade
                 )
-              ) 
+              )
+            ),
+            /* Article Link Button: */
+            Container(
+              decoration: BoxDecoration(
+                gradient: new LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    const Color(0x00FFFFFF), const Color(0xDDFFFFFF), 
+                    const Color(0xEEFFFFFF), const Color(0xFFFFFFFF),
+                    const Color(0xFFFFFFFF)
+                  ],
+                  tileMode: TileMode.repeated, // repeats the gradient over the canvas
+                  stops: [0.0, 0.25, 0.5, 0.75, 1.0]
+                ),
+              ),
+              width: double.infinity,
+              height: 90.0,
+              child: Center(
+                child: RaisedButton(
+                  onPressed: () async {
+                    if (await canLaunch(widget.newsItem.fullArticleLink)) {
+                      await launch(widget.newsItem.fullArticleLink);
+                    } else {
+                      throw 'Could not launch ${widget.newsItem.fullArticleLink}';
+                    }
+                  },
+                  padding: new EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
+                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                  textColor: Colors.black,
+                  color: Colors.white.withOpacity(1.0),
+                  child: Text(
+                    'View Article',
+                    style: TextStyle(
+                      color: const Color(0xFF2980b9),
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.w500
+                    )
+                  )
+                ) 
+              )
             )
-          )
-        ]
+          ]
+        )
       )
     );
   }
