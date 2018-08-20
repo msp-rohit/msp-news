242dd0848c304919e22a8654ea01fa78ac097eea
Pushkar DK <pushkar@mysmartprice.com>
2018-08-20 16:27:44 +0530

    Added splash screen

--- lib/cards/all-cards.dart
+++ lib/cards/all-cards.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import '../model/news-model.dart';
 import 'cards-builder.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
 
-class Cards extends StatelessWidget {
+class Cards extends StatefulWidget {
+  @override
+  _CardsState createState() => _CardsState();
+}
+
+class _CardsState extends State<Cards> {
   List formatSnapshot(ssData) {
     List data = [];
     for(int i = 0; i < ssData.documents.length; i++) {
@@ -20,26 +26,90 @@ class Cards extends StatelessWidget {
     }
     return data;
   }
+  
+  Widget splashScreen(context, showLoader) {
+    String splashImage = 'images/splash_bg@2x.png';
+    String splashHeader = 'images/splash_hdr@2x.png';
 
-  @override
-  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(splashImage),
+          fit: BoxFit.cover,
+        )
+      ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.15,
+            left: 40.0,
+            width: 222.0,
+            height: 71.0,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(splashHeader),
+                  fit: BoxFit.cover,
+                )
+              )
+            )
+          ),
+          showLoader ? Center(
+            child: CircularProgressIndicator() // By default, show a loading spinner
+          ) : Text('')
+        ],
+      )
+    );
+  }
+
+  Widget allCards(context) {
     return Container(
       child: StreamBuilder(
         stream: Firestore.instance.collection('news').snapshots(),
         builder: (context, snapshot) {
-          if(!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator() // By default, show a loading spinner
-            );
-          } else if(snapshot.hasError) {
-            return Center(
-              child: Text("${snapshot.error}")
-            );
-          } else {
-            return CardsBuilder(cards: formatSnapshot(snapshot.data));
+          if(snapshot.connectionState == ConnectionState.none) {
+            return splashScreen(context, false);
+          }
+          if(snapshot.connectionState == ConnectionState.waiting) {
+            return splashScreen(context, true);
+          }
+          if(snapshot.connectionState == ConnectionState.active || 
+            snapshot.connectionState == ConnectionState.done) {
+            if(!snapshot.hasData) {
+              return splashScreen(context, false);
+            } else if(snapshot.hasError) {
+              return Center(
+                child: Text("${snapshot.error}")
+              );
+            } else {
+              return CardsBuilder(cards: formatSnapshot(snapshot.data));
+            }
           }
         }
       )
     );
+  }
+
+  bool splash;
+  @override
+  void initState() {
+    super.initState();
+    splash = true;
+  }
+  void handleTimeout() {
+    setState(() {
+      splash = false;
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    if(splash) {
+      var duration = const Duration(seconds: 3);
+      new Timer(duration, handleTimeout);
+    }
+    return splash ? splashScreen(context, false) : allCards(context);
   }
 }
