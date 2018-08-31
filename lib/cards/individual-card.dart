8729c4bfa2bf7802800dc9705117a406fe98240c
Pushkar DK <pushkar@mysmartprice.com>
2018-08-31 17:15:45 +0530

    Image on article fixed to 50% using flex

--- lib/cards/individual-card.dart
+++ lib/cards/individual-card.dart
@@ -92,120 +92,127 @@ class _IndividualCardState extends State<IndividualCard> with SingleTickerProvid
         children: <Widget>[
           /* Image: */
           Flexible(
-            flex: 3,
+            flex: 4,
             child: Container(
               constraints: BoxConstraints.expand(),
               child:loadImageWithoutErr()
             )
           ),
           /* News Source: */
-          Container(
-            padding: new EdgeInsets.only(top: 10.0, bottom: 5.0, right: 10.0, left: 10.0),
-            width: double.infinity,
-            child: Text(widget.newsItem.sourceName, style: TextStyle(
-                fontSize: 11.0,
-                fontFamily: 'Roboto'
-              ),
-              overflow: TextOverflow.fade
-            )
-          ),
-          /* News Title: */
-          Container(
-            padding: new EdgeInsets.only(bottom: 5.0, left: 10.0, right: 10.0),
-            width: double.infinity,
-            child: Text(widget.newsItem.title, style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.w500
-              ),
-              maxLines: 3,
-              overflow: TextOverflow.ellipsis
-            )
-          ),
-          /* Publish Date: */
-          Container(
-            padding: new EdgeInsets.only(bottom: 10.0, right: 10.0, left: 10.0),
-            width: double.infinity,
-            child: Row(
+          Flexible(
+            flex: 4,
+            child: Column(
               children: <Widget>[
-                Icon(const IconData(0xe8b5, fontFamily: 'MaterialIcons'), size: 11.0),
-                Padding(padding: new EdgeInsets.all(1.0)),
-                Text(humanDate, style: TextStyle(
-                    fontSize: 11.0,
-                    fontFamily: 'Roboto'
-                  ), 
-                  overflow: TextOverflow.fade
+                Container(
+                  padding: new EdgeInsets.only(top: 10.0, bottom: 5.0, right: 10.0, left: 10.0),
+                  width: double.infinity,
+                  child: Text(widget.newsItem.sourceName, style: TextStyle(
+                      fontSize: 11.0,
+                      fontFamily: 'Roboto'
+                    ),
+                    overflow: TextOverflow.fade
+                  )
+                ),
+                /* News Title: */
+                Container(
+                  padding: new EdgeInsets.only(bottom: 5.0, left: 10.0, right: 10.0),
+                  width: double.infinity,
+                  child: Text(widget.newsItem.title, style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500
+                    ),
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis
+                  )
+                ),
+                /* Publish Date: */
+                Container(
+                  padding: new EdgeInsets.only(bottom: 10.0, right: 10.0, left: 10.0),
+                  width: double.infinity,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(const IconData(0xe8b5, fontFamily: 'MaterialIcons'), size: 11.0),
+                      Padding(padding: new EdgeInsets.all(1.0)),
+                      Text(humanDate, style: TextStyle(
+                          fontSize: 11.0,
+                          fontFamily: 'Roboto'
+                        ), 
+                        overflow: TextOverflow.fade
+                      )
+                    ],
+                  ),
+                ),
+                /* Description: */
+                Expanded(
+                  child: Container(
+                    padding: new EdgeInsets.only(bottom: 15.0, right: 10.0, left: 10.0),
+                    width: double.infinity,
+                    child: Text(widget.newsItem.description, style: TextStyle(
+                        fontSize: 15.0,
+                        height: 1.3,
+                        fontFamily: 'Roboto'
+                      ),
+                      overflow: TextOverflow.fade
+                    )
+                  )
+                ),
+                /* Article Link Button: */
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: new LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        const Color(0x00FFFFFF), const Color(0xDDFFFFFF), 
+                        const Color(0xEEFFFFFF), const Color(0xFFFFFFFF),
+                        const Color(0xFFFFFFFF)
+                      ],
+                      tileMode: TileMode.repeated, // repeats the gradient over the canvas
+                      stops: [0.0, 0.25, 0.5, 0.75, 1.0]
+                    ),
+                  ),
+                  width: double.infinity,
+                  height: 90.0,
+                  child: Center(
+                    child: RaisedButton(
+                      onPressed: () {
+                        widget.webViewShow();
+                        final flutterWebviewPlugin = new FlutterWebviewPlugin();
+                        flutterWebviewPlugin.launch(
+                          widget.newsItem.fullArticleLink,
+                          rect: new Rect.fromLTWH(
+                            0.0, 
+                            72.0, 
+                            MediaQuery.of(context).size.width, 
+                            MediaQuery.of(context).size.height
+                          )
+                        );
+                      },
+                      padding: new EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
+                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                      textColor: Colors.black,
+                      color: Colors.white.withOpacity(1.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                          'View Article',
+                            style: TextStyle(
+                              color: const Color(0xFF603C00),
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.w500
+                            )
+                          ),
+                          Container(
+                            padding: new EdgeInsets.only(top: 1.5),
+                            child: Icon(const IconData(0xe409, fontFamily: 'MaterialIcons'), color: const Color(0xFF603C00), size: 14.0)
+                          )
+                        ]
+                      )
+                    ) 
+                  )
                 )
               ],
-            ),
-          ),
-          /* Description: */
-          Expanded(
-            child: Container(
-              padding: new EdgeInsets.only(bottom: 15.0, right: 10.0, left: 10.0),
-              width: double.infinity,
-              child: Text(widget.newsItem.description, style: TextStyle(
-                  fontSize: 15.0,
-                  height: 1.3,
-                  fontFamily: 'Roboto'
-                ),
-                overflow: TextOverflow.fade
-              )
-            )
-          ),
-          /* Article Link Button: */
-          Container(
-            decoration: BoxDecoration(
-              gradient: new LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  const Color(0x00FFFFFF), const Color(0xDDFFFFFF), 
-                  const Color(0xEEFFFFFF), const Color(0xFFFFFFFF),
-                  const Color(0xFFFFFFFF)
-                ],
-                tileMode: TileMode.repeated, // repeats the gradient over the canvas
-                stops: [0.0, 0.25, 0.5, 0.75, 1.0]
-              ),
-            ),
-            width: double.infinity,
-            height: 90.0,
-            child: Center(
-              child: RaisedButton(
-                onPressed: () {
-                  widget.webViewShow();
-                  final flutterWebviewPlugin = new FlutterWebviewPlugin();
-                  flutterWebviewPlugin.launch(
-                    widget.newsItem.fullArticleLink,
-                    rect: new Rect.fromLTWH(
-                      0.0, 
-                      72.0, 
-                      MediaQuery.of(context).size.width, 
-                      MediaQuery.of(context).size.height
-                    )
-                  );
-                },
-                padding: new EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
-                shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-                textColor: Colors.black,
-                color: Colors.white.withOpacity(1.0),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text(
-                    'View Article',
-                      style: TextStyle(
-                        color: const Color(0xFF603C00),
-                        fontSize: 12.0,
-                        fontWeight: FontWeight.w500
-                      )
-                    ),
-                    Container(
-                      padding: new EdgeInsets.only(top: 1.5),
-                      child: Icon(const IconData(0xe409, fontFamily: 'MaterialIcons'), color: const Color(0xFF603C00), size: 14.0)
-                    )
-                  ]
-                )
-              ) 
             )
           )
         ]
